61dc23283d2679b64fda9110ec3b4e4387daac43
shkey325 <shkey325@gmail.com>
2018-07-15 01:01:36 +0800

    合并功能模块，统一程序入口

---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import prettytable
+import login
+import judge
+import score
+
+def main():
+    student = login.Student()
+    student.login()
+    pt = prettytable.PrettyTable(["操作类型"])
+    # 设置prettytable靠左对齐
+    pt.align = "l"
+    pt.add_row(["1、成绩查询"])
+    pt.add_row(["2、一键评教"])
+    print(pt)
+    choice = int(input("请输入你要进行的操作类型：")) - 1
+    if choice == 0:
+        score.main(student)
+    elif choice == 1:
+        judge.judge_all(student)
+    else:
+        print("你的输入有误，请重新运行本程序再次进行输入！")
+        return 0
+
+if __name__ == "__main__":
+    main()
